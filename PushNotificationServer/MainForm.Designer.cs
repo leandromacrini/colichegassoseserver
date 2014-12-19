02da46fe37a4b942e64693207122c56da07a3bfd
leandromacrini@gmail.com <leandromacrini@gmail.com>
2014-12-19 11:58:41 +0100

    asd

--- PushNotificationServer/MainForm.Designer.cs
+++ PushNotificationServer/MainForm.Designer.cs
@@ -32,24 +32,21 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.trayMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.openServerConsoleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.gridConfigurations = new System.Windows.Forms.DataGridView();
             this.btnNew = new System.Windows.Forms.Button();
-            this.btnEdit = new System.Windows.Forms.Button();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.btnDelete = new System.Windows.Forms.Button();
-            this.btnStop = new System.Windows.Forms.Button();
+            this.gridConfigurations = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Status = new System.Windows.Forms.DataGridViewImageColumn();
             this.ConfigName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Errors = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Pushes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Errors = new System.Windows.Forms.DataGridViewImageColumn();
             this.Start = new System.Windows.Forms.DataGridViewButtonColumn();
             this.Stop = new System.Windows.Forms.DataGridViewButtonColumn();
             this.Edit = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Delete = new System.Windows.Forms.DataGridViewButtonColumn();
             this.trayMenu.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridConfigurations)).BeginInit();
@@ -72,18 +69,6 @@
             this.trayMenu.Name = "trayMenu";
             this.trayMenu.Size = new System.Drawing.Size(185, 54);
             // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(181, 6);
-            // 
             // openServerConsoleToolStripMenuItem
             // 
             this.openServerConsoleToolStripMenuItem.Name = "openServerConsoleToolStripMenuItem";
@@ -91,12 +76,20 @@
             this.openServerConsoleToolStripMenuItem.Text = "Open Server Console";
             this.openServerConsoleToolStripMenuItem.Click += new System.EventHandler(this.openServerConsoleToolStripMenuItem_Click);
             // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(181, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.btnStop);
-            this.groupBox1.Controls.Add(this.btnDelete);
-            this.groupBox1.Controls.Add(this.btnStart);
-            this.groupBox1.Controls.Add(this.btnEdit);
             this.groupBox1.Controls.Add(this.btnNew);
             this.groupBox1.Controls.Add(this.gridConfigurations);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
@@ -105,6 +98,44 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Available Push Configurations";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(197, 489);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(121, 43);
+            this.btnNew.TabIndex = 0;
+            this.btnNew.Text = "New Configuration";
+            this.btnNew.UseVisualStyleBackColor = true;
+            // 
+            // gridConfigurations
+            // 
+            this.gridConfigurations.AllowUserToAddRows = false;
+            this.gridConfigurations.AllowUserToDeleteRows = false;
+            this.gridConfigurations.AllowUserToResizeColumns = false;
+            this.gridConfigurations.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridConfigurations.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gridConfigurations.ColumnHeadersHeight = 30;
+            this.gridConfigurations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.gridConfigurations.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Status,
+            this.ConfigName,
+            this.Pushes,
+            this.Errors,
+            this.Start,
+            this.Stop,
+            this.Edit,
+            this.Delete});
+            this.gridConfigurations.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.gridConfigurations.Location = new System.Drawing.Point(0, 19);
+            this.gridConfigurations.MultiSelect = false;
+            this.gridConfigurations.Name = "gridConfigurations";
+            this.gridConfigurations.ReadOnly = true;
+            this.gridConfigurations.RowHeadersVisible = false;
+            this.gridConfigurations.Size = new System.Drawing.Size(514, 464);
+            this.gridConfigurations.TabIndex = 0;
             // 
             // groupBox2
             // 
@@ -115,122 +146,71 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Server Statistics";
             // 
-            // gridConfigurations
-            // 
-            this.gridConfigurations.AllowUserToAddRows = false;
-            this.gridConfigurations.AllowUserToDeleteRows = false;
-            this.gridConfigurations.AllowUserToResizeColumns = false;
-            this.gridConfigurations.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.gridConfigurations.ColumnHeadersHeight = 30;
-            this.gridConfigurations.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.gridConfigurations.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ConfigName,
-            this.Status,
-            this.Errors,
-            this.Pushes,
-            this.Start,
-            this.Stop,
-            this.Edit});
-            this.gridConfigurations.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
-            this.gridConfigurations.Location = new System.Drawing.Point(6, 19);
-            this.gridConfigurations.MultiSelect = false;
-            this.gridConfigurations.Name = "gridConfigurations";
-            this.gridConfigurations.ReadOnly = true;
-            this.gridConfigurations.RowHeadersVisible = false;
-            this.gridConfigurations.Size = new System.Drawing.Size(514, 464);
-            this.gridConfigurations.TabIndex = 0;
-            // 
-            // btnNew
-            // 
-            this.btnNew.Location = new System.Drawing.Point(6, 489);
-            this.btnNew.Name = "btnNew";
-            this.btnNew.Size = new System.Drawing.Size(121, 43);
-            this.btnNew.TabIndex = 0;
-            this.btnNew.Text = "New Configuration";
-            this.btnNew.UseVisualStyleBackColor = true;
-            // 
-            // btnEdit
-            // 
-            this.btnEdit.Location = new System.Drawing.Point(137, 489);
-            this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(121, 43);
-            this.btnEdit.TabIndex = 1;
-            this.btnEdit.Text = "Edit Configuration";
-            this.btnEdit.UseVisualStyleBackColor = true;
-            // 
-            // btnStart
-            // 
-            this.btnStart.Location = new System.Drawing.Point(379, 489);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(43, 43);
-            this.btnStart.TabIndex = 2;
-            this.btnStart.Text = ">";
-            this.btnStart.UseVisualStyleBackColor = true;
-            // 
-            // btnDelete
-            // 
-            this.btnDelete.Location = new System.Drawing.Point(477, 489);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(43, 43);
-            this.btnDelete.TabIndex = 3;
-            this.btnDelete.Text = "X";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            // 
-            // btnStop
-            // 
-            this.btnStop.Location = new System.Drawing.Point(428, 489);
-            this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(43, 43);
-            this.btnStop.TabIndex = 4;
-            this.btnStop.Text = "[ ]";
-            this.btnStop.UseVisualStyleBackColor = true;
-            // 
-            // Name
-            // 
-            this.ConfigName.HeaderText = "Name";
-            this.ConfigName.MinimumWidth = 250;
-            this.ConfigName.Name = "Name";
-            this.ConfigName.ReadOnly = true;
-            this.ConfigName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
-            this.ConfigName.Width = 250;
-            // 
             // Status
             // 
             this.Status.HeaderText = "Status";
+            this.Status.MinimumWidth = 40;
             this.Status.Name = "Status";
             this.Status.ReadOnly = true;
+            this.Status.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Status.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
-            // Errors
+            // ConfigName
             // 
-            this.Errors.HeaderText = "Errors";
-            this.Errors.Name = "Errors";
-            this.Errors.ReadOnly = true;
+            this.ConfigName.HeaderText = "Configuration Name";
+            this.ConfigName.MinimumWidth = 50;
+            this.ConfigName.Name = "ConfigName";
+            this.ConfigName.ReadOnly = true;
+            this.ConfigName.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Pushes
             // 
             this.Pushes.HeaderText = "Pushes";
+            this.Pushes.MinimumWidth = 80;
             this.Pushes.Name = "Pushes";
             this.Pushes.ReadOnly = true;
+            this.Pushes.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Errors
+            // 
+            this.Errors.HeaderText = "Errors";
+            this.Errors.MinimumWidth = 40;
+            this.Errors.Name = "Errors";
+            this.Errors.ReadOnly = true;
+            this.Errors.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Errors.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
             // 
             // Start
             // 
             this.Start.HeaderText = "Start";
+            this.Start.MinimumWidth = 40;
             this.Start.Name = "Start";
             this.Start.ReadOnly = true;
+            this.Start.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Stop
             // 
             this.Stop.HeaderText = "Stop";
+            this.Stop.MinimumWidth = 40;
             this.Stop.Name = "Stop";
             this.Stop.ReadOnly = true;
+            this.Stop.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // Edit
             // 
             this.Edit.HeaderText = "Edit";
+            this.Edit.MinimumWidth = 40;
             this.Edit.Name = "Edit";
             this.Edit.ReadOnly = true;
+            this.Edit.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Delete
+            // 
+            this.Delete.HeaderText = "Delete";
+            this.Delete.MinimumWidth = 40;
+            this.Delete.Name = "Delete";
+            this.Delete.ReadOnly = true;
+            this.Delete.Resizable = System.Windows.Forms.DataGridViewTriState.False;
             // 
             // MainForm
             // 
@@ -263,20 +243,17 @@
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button btnStop;
-        private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.DataGridView gridConfigurations;
+        private System.Windows.Forms.DataGridViewImageColumn Status;
         private System.Windows.Forms.DataGridViewTextBoxColumn ConfigName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Errors;
         private System.Windows.Forms.DataGridViewTextBoxColumn Pushes;
+        private System.Windows.Forms.DataGridViewImageColumn Errors;
         private System.Windows.Forms.DataGridViewButtonColumn Start;
         private System.Windows.Forms.DataGridViewButtonColumn Stop;
         private System.Windows.Forms.DataGridViewButtonColumn Edit;
-        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridViewButtonColumn Delete;
     }
 }
 
