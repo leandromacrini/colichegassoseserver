ea9ecca8055e3ea139f5afa89e467e45f13ac9fe
leandromacrini@gmail.com <leandromacrini@gmail.com>
2015-02-19 11:25:32 +0100

    asd

---
+++ ColicheGassose/Controllers/StatisticsController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ColicheGassose.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login", "Statistics");
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("ShowStatistics", "Statistics");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Login(WebUser webuser)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Validate(webuser))
+                {
+                    FormsAuthentication.SetAuthCookie(webuser.User, false);
+                    return RedirectToAction("ShowStatistics", "Statistics");
+                }
+                else
+                {
+                    ModelState.AddModelError("Errors", "Dati di accesso errati!");
+                }
+            }
+            return View(webuser);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize]
+        public ActionResult ShowStatistics()
+        {
+            return View();
+        }
+
+        private bool Validate(WebUser user)
+        {
+            return true;
+        }
+    }
+}
